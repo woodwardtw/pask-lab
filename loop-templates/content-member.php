548f31d99acca2078b1d8a33ce612adeead957ae
woodwardtw <twwoodward@me.com>
2022-02-04 14:42:24 -0500

    single member work

--- loop-templates/content-member.php
+++ loop-templates/content-member.php
@@ -11,20 +11,18 @@ defined( 'ABSPATH' ) || exit;
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
 
-		<?php the_title( '<h1 class="entry-title">member', '</h1>' ); ?>
-
-		<div class="entry-meta">
-
-			<?php understrap_posted_on(); ?>
-
-		</div><!-- .entry-meta -->
-
-	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
+<div class="row">
+	<div class="col-md-5 black">
+		<div class="member-img-holder">
+			<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+		</div>
+	</div>
+	<div class="col-md-7 black">
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+	</div>
 	<div class="entry-content">
 
 		<?php
--- single-member.php
+++ single-member.php
@@ -15,7 +15,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="single-wrapper">
 
-	<div class="container-fluid" id="content" tabindex="-1">
+	<div class="container-fluid" id="member-content" tabindex="-1">
 
 		<div class="row">
 
