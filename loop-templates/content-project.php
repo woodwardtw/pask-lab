ead113c255ab5177f6336631aab2e2919015efea
woodwardtw <twwoodward@me.com>
2022-02-10 13:44:31 -0500

    associate members w projects via acf

--- loop-templates/content-project.php
+++ loop-templates/content-project.php
@@ -15,12 +15,6 @@ defined( 'ABSPATH' ) || exit;
 
 		<?php the_title( '<h1 class="entry-title">proj', '</h1>' ); ?>
 
-		<div class="entry-meta">
-
-			<?php understrap_posted_on(); ?>
-
-		</div><!-- .entry-meta -->
-
 	</header><!-- .entry-header -->
 
 	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
